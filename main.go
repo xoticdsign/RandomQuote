2863ebbf7043ac86e3de55b1e322eb37c12cde9b
xoti$ <xoticdollarsign@outlook.com>
2024-07-18 01:51:54 +0300

    Finishing formatting output

---
+++ main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Quote struct {
+	Author   string `Author:"author"`
+	Category string `Category:"category"`
+	Text     string `Text:"text"`
+}
+
+var quote [1]Quote
+
+func main() {
+	url := "https://famous-quotes4.p.rapidapi.com/random?category=all&count=2"
+
+	req, errRequesting := http.NewRequest("GET", url, nil)
+	if errRequesting != nil {
+		log.Println("Failed to make a request")
+	}
+
+	req.Header.Add("x-rapidapi-key", "8d6ceaae3fmsh77482db1c42818ep137cb8jsn6e55c2d0bf2e")
+	req.Header.Add("x-rapidapi-host", "famous-quotes4.p.rapidapi.com")
+
+	res, errReceiving := http.DefaultClient.Do(req)
+	if errReceiving != nil {
+		log.Println("Failed to receive a response")
+	}
+
+	defer res.Body.Close()
+
+	response, errReading := io.ReadAll(res.Body)
+	if errReading != nil {
+		log.Println("Failed to read response")
+	} else {
+
+	}
+
+	errUnmarshaling := json.Unmarshal(response, &quote)
+	if errUnmarshaling != nil {
+		log.Println("Failed to unmarshal")
+	}
+
+	for _, output := range quote {
+		fmt.Printf("\nAuthor: %v\nCategory: %v\nQuote: %v\n", output.Author, output.Category, output.Text)
+	}
+
+}
