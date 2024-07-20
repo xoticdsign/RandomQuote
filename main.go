7c14359a5a5c64ff476ca966b3681e6384695b77
Roman Epishov <kutaisi@bentley.local>
2024-07-20 16:19:56 +0300

    Structured code

--- main.go
+++ main.go
@@ -10,6 +10,36 @@ import (
 	"github.com/iancoleman/strcase"
 )
 
+func main() {
+	QuoteAPI()
+}
+
+func QuoteAPI() {
+	url := "https://famous-quotes4.p.rapidapi.com/random?category=all&count=2"
+
+	req, errRequestingQuote := http.NewRequest("GET", url, nil)
+	if errRequestingQuote != nil {
+		log.Println("Failed to make a request")
+	}
+
+	req.Header.Add("x-rapidapi-key", "8d6ceaae3fmsh77482db1c42818ep137cb8jsn6e55c2d0bf2e")
+	req.Header.Add("x-rapidapi-host", "famous-quotes4.p.rapidapi.com")
+
+	res, errReceivingQuote := http.DefaultClient.Do(req)
+	if errReceivingQuote != nil {
+		log.Println("Failed to receive a response")
+	}
+
+	defer res.Body.Close()
+
+	response, errReadingQuote := io.ReadAll(res.Body)
+	if errReadingQuote != nil {
+		log.Println("Failed to read response")
+	}
+
+	QuoteJSON(&response)
+}
+
 type Quote struct {
 	Author   string `Author:"author"`
 	Category string `Category:"category"`
@@ -18,31 +48,9 @@ type Quote struct {
 
 var quote [1]Quote
 
-func main() {
-	url := "https://famous-quotes4.p.rapidapi.com/random?category=all&count=2"
-
-	req, errRequesting := http.NewRequest("GET", url, nil)
-	if errRequesting != nil {
-		log.Println("Failed to make a request")
-	}
-
-	req.Header.Add("x-rapidapi-key", "8d6ceaae3fmsh77482db1c42818ep137cb8jsn6e55c2d0bf2e")
-	req.Header.Add("x-rapidapi-host", "famous-quotes4.p.rapidapi.com")
-
-	res, errReceiving := http.DefaultClient.Do(req)
-	if errReceiving != nil {
-		log.Println("Failed to receive a response")
-	}
-
-	defer res.Body.Close()
-
-	response, errReading := io.ReadAll(res.Body)
-	if errReading != nil {
-		log.Println("Failed to read response")
-	}
-
-	errUnmarshaling := json.Unmarshal(response, &quote)
-	if errUnmarshaling != nil {
+func QuoteJSON(response *[]byte) {
+	errUnmarshalingQuote := json.Unmarshal(*response, &quote)
+	if errUnmarshalingQuote != nil {
 		log.Println("Failed to unmarshal")
 	}
 
